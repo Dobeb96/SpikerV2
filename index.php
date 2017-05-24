d67d804646daa8b06fcbc7f46fb683d035cf97ef
dobeb <maciej.dobosz_md@outlook.com>
2017-05-24 20:33:46 +0200

    Return the list of all lectures from the server added

--- index.php
+++ index.php
@@ -11,9 +11,23 @@
             <p class="sub_logo">Twoje centrum wymiany notatek</p>
         </header>
         
-        <!-- CONTENT POBIERANY Z SERWERA-->
+        <!-- CONTENT POBIERANY Z SERWERA -->
         <section>
-            <!-- tutaj pobieranie listy przedmiotow przez php -->
+            <?php
+            // glob() przeszuka pliki na serwerze w poszukiwaniu podanego patternu ("./*") i zwroci je jako tablica; drugi parametr oznacza znajdowanie tylko katalogow
+            $directories = glob("./*", GLOB_ONLYDIR);
+                
+            foreach ($directories as $dir) {                
+                // dla kazdego katalogu wejdz do niego i otworz plik name.txt do odczytu ("r")
+                // w pliku name.txt znajduja sie nazwy przedmiotow z duzej litery i bez podkreslnikow
+                if( $file = fopen($dir."/name.txt", "r") ) {
+                    // wydrukuj zawartosc pliku
+                    print fgets($file)."<br>";
+
+                    fclose($file);   
+                }
+            }
+            ?>
         </section>
         
         <!-- STOPKA WITRYNY -->
