8a9535574a28314a2d62a6a7286552d3a9c5a81b
dobeb <maciej.dobosz_md@outlook.com>
2017-05-24 18:14:21 +0200

    initial version of index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <title>Spiker!</title>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
+        <!-- LOGO WITRYNY -->
+        <header>
+            <!-- tutaj logo i wszelkie informacje potrzebne w naglowku witryny -->
+        </header>
+        
+        <!-- CONTENT POBIERANY Z SERWERA-->
+        <section>
+            <!-- tutaj pobieranie listy przedmiotow przez php -->
+        </section>
+        
+        <!-- STOPKA WITRYNY -->
+        <footer>
+            <!-- tutaj dodatkowe informacje np. zrzeczenie sie praw autorskich czy inne -->
+        </footer>
+    </body>
+</html>
