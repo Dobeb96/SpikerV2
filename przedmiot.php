1f3a5127fc386617c27d0899ec4a0f2dc89762fb
dobeb <maciej.dobosz_md@outlook.com>
2017-05-26 16:17:25 +0200

    Added style to przedmiot.php

--- przedmiot.php
+++ przedmiot.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
 <html lang="pl-PL">
     <head>
-        <title>Spiker!</title>
+        <title>Spiker</title>
         <meta charset="UTF-8" />
+        <link href="mainstyle.css" rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato">
     </head>
     <body>
-        <!-- LOGO WITRYNY -->
-        <header>
-            <h1 class="main_logo">Spiker</h1>
-            <p class="sub_logo">Twoje centrum wymiany notatek</p>
-        </header>
+        <div id="container">
+            
+            <!-- LOGO WITRYNY -->
+            <div id="header">
+                <h1 class="main_logo">Spiker</h1>
+                <p class="sub_logo">Twoje centrum wymiany notatek</p>
+            </div>  
         
-        <!-- CONTENT POBIERANY Z SERWERA -->
-        <section>
-        <?php
-        if (isset ($_GET['id']))
-            $id = $_GET['id'];
+            <!-- CONTENT POBIERANY Z SERWERA -->
+            <section>
+            <?php
+            if (isset ($_GET['id']))
+                $id = $_GET['id'];
 
-        $directories = glob("./*", GLOB_ONLYDIR);
+            $directories = glob("./*", GLOB_ONLYDIR);
 
-        print $directories[$id];
+            print $directories[$id];
 
-        ?> 
-        </section>
-        
-        <!-- STOPKA WITRYNY -->
-        <footer>
-            <!-- tutaj dodatkowe informacje np. zrzeczenie sie praw autorskich czy inne -->
-        </footer>
+            ?> 
+            </section>
+
+            <!-- STOPKA WITRYNY -->
+            <footer>
+                <!-- tutaj dodatkowe informacje np. zrzeczenie sie praw autorskich czy inne -->
+            </footer>
+        </div> <!-- end container -->
     </body>
 </html>
