ff68cfa2bcbd05158a2d878f6d16629fb932e548
dobeb <maciej.dobosz_md@outlook.com>
2017-06-12 22:30:00 +0200

    Version 1.0.3.1 style in chrome will be now updated

--- ad.php
+++ ad.php
@@ -4,7 +4,7 @@
         <title>Spiker</title>
         <meta charset="UTF-8" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <link href="mainstyle.css" rel="stylesheet" type="text/css">
+        <link href="mainstyle.css?v=1.0.3" rel="stylesheet" type="text/css">
         <link rel="stylesheet"  href="//fonts.googleapis.com/css?family=Lato&effect=anaglyph">
     </head>
     <body>
