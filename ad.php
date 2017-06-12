24c77cdc8ed6229c0314e0d156c556a68c5b5ce1
dobeb <maciej.dobosz_md@outlook.com>
2017-06-12 22:26:41 +0200

    Version 1.0.3 iframe ads added

--- ad.php
+++ ad.php
@@ -3,6 +3,7 @@
     <head>
         <title>Spiker</title>
         <meta charset="UTF-8" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <link href="mainstyle.css" rel="stylesheet" type="text/css">
         <link rel="stylesheet"  href="//fonts.googleapis.com/css?family=Lato&effect=anaglyph">
     </head>
@@ -28,14 +29,25 @@
             
             <table class="ad_table" id="ad_table_id1">
             <tr>
-                <td>
+                <td align="center">
                     <!-- REKLAMA -->
-                    <div class="ad_container" onClick="ad_clicked(this)">
-                        <a href="https://www.ceneo.pl/Laptopy_i_komputery#pid=14418&crid=120963&cid=22167" rel="nofollow" target="_blank"><img src="https://image2.ceneo.pl/data/banners/banner_373.jpg" /></a>
-                    </div>
+                        <script type="text/javascript">
+                          ( function() {
+                            if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
+                            var unit = {"calltype":"async[2]","publisher":"doberman96","width":300,"height":250,"sid":"Chitika Default"};
+                            var placement_id = window.CHITIKA.units.length;
+                            window.CHITIKA.units.push(unit);
+                            document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
+                        }());
+                        </script>
+                        <script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
                 </td>
             </tr>
-              <div id="bottom"></div>
+            <tr>
+                <td>
+                    <div id="bottom"></div>
+                </td>    
+            </tr>
             <tr>                
                 <td>
                     <div class="download_button_active" id="up" style="width: 625px;"><p>Pobierz</p></div>
@@ -62,9 +74,9 @@
             </tr>
             </table>
             
-            <!-- SPRAWDZ ADBLOCKA -->
             <script src="./ads.js" type="text/javascript"></script>
             <script>
+            // SPRAWDZ ADBLOCKA
             if(document.getElementById('XNPIiaZmxEev')){
               var doNothing = true;
             } else {
@@ -72,15 +84,35 @@
                 document.getElementById('ad_table_id1').innerHTML = "<br><br><font color='red'>Używasz adblocka, proszę rozważ wyłączenie go dla tej strony, bo reklamy to jedyne źródło opłacenia naszego serwera</font>";
                 document.getElementById('bottom').setAttribute("class", "download_button_inactive");
             }
-            </script>
             
-            <script>
-            function ad_clicked(source) {
+            // IFRAME CLICK MONITORING
+            var monitor = setInterval(function(){
+                var elem = document.activeElement;
+                if(elem && elem.tagName == 'IFRAME'){
+                    ad_clicked();
+                    clearInterval(monitor);
+                }
+            }, 100);
+            // END IFRAME
+                
+            function ad_clicked() {
                 document.getElementById('up').setAttribute("class", "download_button_inactive");
                 document.getElementById('down').setAttribute("class", "download_button_active");
             }
             </script>
             
         </div> <!-- END container -->
+        
+        <!-- GOOGLE ANALYTICS -->
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-86627205-2', 'auto');
+          ga('send', 'pageview');
+        </script>
+        <!-- END GOOGLE ANALYTICS -->
     </body>
 </html>
